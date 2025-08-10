6cbfdc20b37d24ae961cdbea0b670051d1df4ba6
Trang Le <trang.thule@zoho.com>
2025-08-10 15:28:31 +0700

    scoping hours of the day

--- redis-key-generator.js
+++ redis-key-generator.js
@@ -6,8 +6,8 @@ const getEventLogKey = (eventId) => getKey(`event-log:${eventId}`);
 const getRepoScoreKey = () => getKey(`repo-score`);
 const getDevScoreKey = () => {
   const now = new Date();
-  const dateString = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}-${now.getHours()}`;
-  return getKey(`dev-score:${dateString}`);
+  const dateString = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}`;
+  return getKey(`dev-score:${dateString}:${now.getHours()}`);
 }
 
 module.exports = {
